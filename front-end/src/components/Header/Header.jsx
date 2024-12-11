3eba65ba45e6ff929787704f731fcfdf9bb86c21
mangesh0910 <mangeshgosavi300@gmail.com>
2024-12-11 11:21:43 +0530

    Added navbar

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css';
+import Navbar from '../Navbar/Navbar';
 
 const Header = () => {
     return (
         <header>
             <div className='wrapper'>
+                <Navbar />
                 <div className='cta'>
                     <h1 className='course-title'>Sound Design Masterclass</h1>
                     <p className='course-desc'>Learn the art of sound design</p>
